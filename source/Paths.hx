90490e06134f8f818b4e2c9b1516ca7ab2b56553
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-01-06 00:38:03 +0200

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -1,6 +1,7 @@
 package;
 
 import openfl.display3D.textures.Texture;
+import flixel.math.FlxPoint;
 import flixel.graphics.frames.FlxFrame.FlxFrameAngle;
 import openfl.geom.Rectangle;
 import flixel.math.FlxRect;
