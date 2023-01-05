0ddfeaca37bd790c194be60e81271949314af247
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-01-05 22:01:58 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -15,80 +15,33 @@ class Main extends Sprite
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
-	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var zoom:Float = 1; // Zoom of the game.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
 	public static var fpsCounter:FPS;
-	public static var fpsVar:FPS;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
-	public static function main():Void
-	{
-		Lib.current.addChild(new Main());
-	}
-
-	public function new()
+	public function new():Void
 	{
 		super();
 
-		if (stage != null)
-		{
-			init();
-		}
-		else
-		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
-		}
-	}
-
-	private function init(?E:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-
-		setupGame();
-	}
-
-	private function setupGame():Void
-	{
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
-
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
-
-		#if !debug
-		initialState = TitleState;
-		#end
+		SUtil.uncaughtErrorHandler();
 
 		ClientPrefs.startControls();
 
-		// Paths.getModFolders();
+		SUtil.checkPermissions();
+
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		FlxG.signals.gameResized.add(onResizeGame);
 
-		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
-
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		if (fpsCounter != null)
-		{
 			fpsCounter.visible = ClientPrefs.showFPS;
-		}
-		#end
 
 		#if html5
 		FlxG.autoPause = false;
@@ -96,7 +49,7 @@ class Main extends Sprite
 		#end
 	}
 
-	function onResizeGame(w:Int, h:Int)
+	function onResizeGame(w:Int, h:Int):Void
 	{
 		if (FlxG.cameras == null)
 			return;
@@ -104,44 +57,18 @@ class Main extends Sprite
 		for (cam in FlxG.cameras.list)
 		{
 			@:privateAccess
-			if (cam != null && (cam._filters != null || cam._filters != []))
-				fixShaderSize(cam);
-		}
-	}
-
-	function fixShaderSize(camera:FlxCamera) // Shout out to Ne_Eo for bringing this to my attention
-	{
-		@:privateAccess {
-			var sprite:Sprite = camera.flashSprite;
-
-			if (sprite != null)
+			if (cam != null && (cam._filters != null || cam._filters.length > 0))
 			{
-				sprite.__cacheBitmap = null;
-				sprite.__cacheBitmapData = null;
-				sprite.__cacheBitmapData2 = null;
-				sprite.__cacheBitmapData3 = null;
-				sprite.__cacheBitmapColorTransform = null;
+				var sprite:Sprite = cam.flashSprite; // Shout out to Ne_Eo for bringing this to my attention
+				if (sprite != null)
+				{
+					sprite.__cacheBitmap = null;
+					sprite.__cacheBitmapData = null;
+					sprite.__cacheBitmapData2 = null;
+					sprite.__cacheBitmapData3 = null;
+					sprite.__cacheBitmapColorTransform = null;
+				}
 			}
 		}
-	}
-
-	public function toggleFPS(fpsEnabled:Bool):Void
-	{
-		fpsCounter.visible = fpsEnabled;
-	}
-
-	public function setFPSCap(cap:Float)
-	{
-		openfl.Lib.current.stage.frameRate = cap;
-	}
-
-	public function getFPSCap():Float
-	{
-		return openfl.Lib.current.stage.frameRate;
-	}
-
-	public function getFPS():Float
-	{
-		return fpsCounter.currentFPS;
 	}
 }
