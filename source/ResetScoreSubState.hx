274b00481f814747407b292c496e24561f4da583
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-01-06 14:52:22 +0200

    Update ResetScoreSubState.hx

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -71,6 +71,11 @@ class ResetScoreSubState extends MusicBeatSubstate
 		noText.x += 200;
 		add(noText);
 		updateOptions();
+
+		#if mobile
+		addVirtualPad(LEFT_RIGHT, A_B);
+		addVirtualPadCamera();
+		#end
 	}
 
 	override function update(elapsed:Float)
@@ -96,7 +101,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 		if (controls.BACK)
 		{
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
+			#if mobile
+			flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+			#else
 			close();
+			#end
 		}
 		else if (controls.ACCEPT)
 		{
@@ -111,8 +121,14 @@ class ResetScoreSubState extends MusicBeatSubstate
 					Highscore.resetWeek(WeekData.weeksList[week], difficulty);
 				}
 			}
+
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
+			#if mobile
+			flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+			#else
 			close();
+			#end
 		}
 		super.update(elapsed);
 	}
