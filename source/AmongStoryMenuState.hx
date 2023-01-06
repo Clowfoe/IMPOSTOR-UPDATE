f69c60e6ce9982c9f785a3a3549a98012a87bc90
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2023-01-06 14:48:56 +0300

    Update AmongStoryMenuState.hx

--- source/AmongStoryMenuState.hx
+++ source/AmongStoryMenuState.hx
@@ -84,6 +84,10 @@ class AmongStoryMenuState extends MusicBeatState
 
 	function doTheThing()
 	{
+		#if debug
+		unlockedWeek[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11] = true;
+		#end
+
 		if (Highscore.getScore('meltdown', 2) != 0)
 		{
 			unlockedWeek[1] = true;
