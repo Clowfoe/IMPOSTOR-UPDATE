b56573874f8a91cb1d8d70e4db90bdc00e566164
trayfellouch <48515908+trayfellow@users.noreply.github.com>
2023-01-07 11:07:25 +0700

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -302,7 +302,7 @@ class ChartingState extends MusicBeatState
 		if(curSection >= _song.notes.length) curSection = _song.notes.length - 1;
 
 		FlxG.mouse.visible = true;
-		FlxG.save.bind('funkin', 'ninjamuffin99');
+		FlxG.save.bind('vsimpostor', 'impostorm');
 
 		tempBpm = _song.bpm;
 
