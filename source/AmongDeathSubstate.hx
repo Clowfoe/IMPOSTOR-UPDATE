9a5aecca5dfa01c1c77a7c6e2219c4db0e72cef6
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2023-01-08 15:51:00 +0300

    Update AmongDeathSubstate.hx

--- source/AmongDeathSubstate.hx
+++ source/AmongDeathSubstate.hx
@@ -123,7 +123,12 @@ class AmongDeathSubstate extends MusicBeatSubstate
 						FlxG.sound.play(Paths.sound('panelAppear', 'impostor'), 0.5);
 					}
 				}
+<<<<<<< Updated upstream
 			});
+=======
+			}
+		);
+>>>>>>> Stashed changes
 		}
 	}
 
